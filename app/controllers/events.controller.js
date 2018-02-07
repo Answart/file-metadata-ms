aee3014a0b9fa9d8ea4816bbcc1cbeb221efb340
Alexandra Swart <answart@sbcglobal.net>
2018-02-06 19:07:31 -0800

    Use multer package to process uploaded file

--- app/controllers/events.controller.js
+++ app/controllers/events.controller.js
@@ -1,6 +1,26 @@
+const multer          = require('multer'),
+  upload              = multer({
+  	storage: multer.memoryStorage()
+  }).single('imgFile');
+
 
 function showUploadedFile(req, res) {
-  res.status(200).json({});
+  function returnWithErrors(err) {
+    console.log(err);
+    res.render('pages/home', {
+      errors: []
+    });
+  }
+
+  upload(req, res, function(err) {
+    if (err) {
+      returnWithErrors(err);
+    } else if (!req.file) {
+      returnWithErrors('Unable to find uploaded file.');
+    } else {
+      res.status(200).json({});
+    }
+  })
 }
 
 function show404(req, res) {
